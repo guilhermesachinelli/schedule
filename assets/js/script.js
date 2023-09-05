bc52d5e8ec7adf113d7a95686e856478febc6037
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-09-05 08:22:07 -0300

    feat: js linked

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+class contact{
+    constructor(name, landline, phone, imgLink, date, email, cep, city, insta, gitHub){
+        this.name = name;
+        this.landline = landline;
+        this.phone = phone;
+        this.imgLink = imgLink;
+        this.date = date;
+        this.email = email;
+        this.cep = cep;
+        this.city = city;
+        this.insta = insta;
+        this.gitHub = gitHub;
+    }
+}
