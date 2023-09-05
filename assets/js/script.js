9abe147ee9ba79f6591598d77b51f5fe088fa307
guilhermesachinelli <124887055+guilhermesachinelli@users.noreply.github.com>
2023-09-05 08:34:06 -0300

    feat: create functions envieMsg , anyInputs and createContact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-class contact{
+class Contact{
     constructor(name, landline, phone, imgLink, date, email, cep, city, insta, gitHub){
         this.name = name;
         this.landline = landline;
@@ -11,4 +11,69 @@ class contact{
         this.insta = insta;
         this.gitHub = gitHub;
     }
+}
+class ContactList{
+    constructor(){
+        this.contacts = [];
+    }
+    addContact(contact){
+        this.contacts.push(contact);
+        clearFields();
+    }
+}
+const contactList = new ContactList();
+function createContact(){
+    let name = document.getElementById("name").value;
+    let landline = document.getElementById("landline").value;
+    let phone = document.getElementById("phone").value;
+    let imgLink = document.getElementById("imgLink").value;
+    let date = document.getElementById("date").value;
+    let email = document.getElementById("email").value;
+    let cep = document.getElementById("cep").value;
+    let city = document.getElementById("city").value;
+    let insta = document.getElementById("insta").value;
+    let gitHub = document.getElementById("gitHub").value;
+    const contact = new Contact(name, landline, phone, imgLink, date, email, cep, city, insta, gitHub);
+    contactList.addContact(contact);
+    console.log(contactList);
+}
+function clearFields(){
+    document.getElementById("name").value = "";
+    document.getElementById("landline").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("imgLink").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("cep").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("insta").value = "";
+    document.getElementById("gitHub").value = "";
+}
+function anyInputs(){
+    let name = document.getElementById("name").value;
+    let landline = document.getElementById("landline").value;
+    let phone = document.getElementById("phone").value;
+    let imgLink = document.getElementById("imgLink").value;
+    let date = document.getElementById("date").value;
+    let email = document.getElementById("email").value;
+    let cep = document.getElementById("cep").value;
+    let city = document.getElementById("city").value;
+    let insta = document.getElementById("insta").value;
+    let gitHub = document.getElementById("gitHub").value;
+    if(name == "" || landline == "" || phone == "" || imgLink == "" || date == "" || email == "" || cep == "" || city == "" || insta == "" || gitHub == ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+function envieMsg(msg, tipoMsg) {
+
+    let msgDiv = document.getElementById("msg");
+    msgDiv.innerHTML = "";
+    let msgParaTela = `
+     <p class="${tipoMsg}">${msg}</p> `
+    msgDiv.innerHTML += msgParaTela;
+    setTimeout(function () {
+        msgDiv.innerHTML = "";
+    }, 3000);
 }
